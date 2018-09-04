d088881ae107bfa2b665d6f52cee7ed8191a68d3
unknown <junior.neves@outlook.com>
2018-09-04 12:58:03 -0300

    Add Measure model

---
+++ models/Measure.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Junior
+ * Date: 04/09/2018
+ * Time: 12:55
+ */
+
+class Measure
+{
+    public $name;
+    public $metric;
+    public $observation;
+
+}
